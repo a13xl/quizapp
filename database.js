41c58e08855b52ad867db7066faadaf64da1c87b
User <general01@tuta.io>
2023-01-13 22:24:01 +0100

    create questions array

---
+++ database.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        'question': 'Wer hat HTML erfunden?',
+        'answer_1': 'Richard Stallman',
+        'answer_2': 'Tim Berners-Lee',
+        'answer_3': 'Gabe Newell',
+        'answer_4': 'Jerry Yang',
+        'right_answer': 2
+    },
+    {
+        'question': 'Wer hat den ersten, funktionsfähigen, Computer erfunden?',
+        'answer_1': 'Kim Dotcom',
+        'answer_2': 'Steve Jobs',
+        'answer_3': 'William C. Lowe',
+        'answer_4': 'Konrad Zuse',
+        'right_answer': 4
+    },
+    {
+        'question': 'Wer hat das erste Microsoft Betriebssystem geschrieben?',
+        'answer_1': 'Konrad Zuse',
+        'answer_2': 'Bill Gates',
+        'answer_3': 'Gary Kildall',
+        'answer_4': 'Paul Allen',
+        'right_answer': 2
+    },
+    {
+        'question': 'Wer gilt als erste/r Ersteller/in von Computerprogrammen?',
+        'answer_1': 'Steve Wozniak',
+        'answer_2': 'Joan Clarke',
+        'answer_3': 'Ada Lovelace',
+        'answer_4': 'Dennis Ritchie',
+        'right_answer': 3
+    },
+    {
+        'question': 'Welche Programmiersprache gibt es <b>nicht</b>?',
+        'answer_1': 'Eureka',
+        'answer_2': 'C',
+        'answer_3': 'Fortress',
+        'answer_4': 'Brainfuck',
+        'right_answer': 1
+    }
+];
